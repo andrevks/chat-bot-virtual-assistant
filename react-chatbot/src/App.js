f3a818459de41d62438c68f17c6a4a24705fba02
andrevks <italiabk@gmail.com>
2021-12-15 20:37:36 -0400

    build: main design screen

--- react-chatbot/src/App.js
+++ react-chatbot/src/App.js
@@ -1,22 +1,37 @@
-import logo from './logo.svg';
+import React, {useState, useEffect } from 'react';
+import mainPhoto from './mainphoto.png';
+import mic from './dark.png';
 import './App.css';
 
 function App() {
+  const [ currentTime, setCurrentTime ] = useState(0);
+
+  useEffect(()=>{
+    fetch('/time').then(res => res.json()).then(data =>{
+      setCurrentTime(data.time);
+    });
+  }, []);
+
   return (
     <div className="App">
+      <div className=''> </div>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <div style={{
+          position: 'relative',
+          width: '100%',
+          height: '100%',
+          minHeight: '100vh',
+          background: '#375CEF'}}
         >
-          Learn React
-        </a>
+        <img src={mainPhoto} className="mainPhoto" alt="Main Photo"/>
+
+        <img src={mic} className="mic" alt="microphone"/>
+          {/* <p>
+            The current time is {currentTime}.
+          </p> */}
+        </div>
+
       </header>
     </div>
   );
