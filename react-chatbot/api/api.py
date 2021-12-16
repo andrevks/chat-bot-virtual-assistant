f004ac1e4f04e9f6fd340990373358afb0ca6017
andrevks <italiabk@gmail.com>
2021-12-15 21:43:22 -0400

    feat: postMsg from react

--- react-chatbot/api/api.py
+++ react-chatbot/api/api.py
@@ -1,7 +1,10 @@
 import time
-from flask import Flask
+from flask_cors import CORS
+from flask import Flask, request
+
 
 app = Flask(__name__)
+CORS(app)
 
 
 @app.route('/')
@@ -11,3 +14,21 @@ def home():
 @app.route('/time')
 def get_current_time():
     return {'time': time.time()}
+
+@app.route('/record', methods = ['POST'])
+def recordings():
+  if request.method == 'POST':
+    data = request.get_json()
+    voice_clip = data['voice_clip']
+    print(f">>>>>>>>>>>>>>>{data}")
+    return {'data': voice_clip} 
+    # return {'data':'asflkjalfjlasdkjf'}
+  # else:
+  #   data = 'Testing...' if not voice_clip else voice_clip
+  #   return {'voice': data }
+  
+  # data = voice_clip
+  # if data:
+  #   return f"Voice clip received {data}"
+  # else :
+  #   return f"Where is the voice clip?"
--- react-chatbot/src/App.js
+++ react-chatbot/src/App.js
@@ -7,17 +7,40 @@ function App() {
   const [ currentTime, setCurrentTime ] = useState(0);
 
   useEffect(()=>{
-    fetch('/time').then(res => res.json()).then(data =>{
+    fetch('/time',{
+      mode: 'cors'
+    }).then(res => res.json()).then(data =>{
       setCurrentTime(data.time);
     });
   }, []);
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    let voiceClip = {'voice_clip':'No PAIN no GAIN!'};
+    voiceClip = JSON.stringify(voiceClip);
+    fetch('http://localhost:5000/record', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      mode: 'cors',
+      body: voiceClip
+    }).then(res => res.json()).then(data =>{
+      console.log(data);
+    });
+    
+    // alert("You clicked ");
+    // or you can send to backend
+  };
+
 
   return (
     <div className="App">
       <div className=''> </div>
       <header className="App-header">
         {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        <div style={{
+        <div 
+        style={{
           position: 'relative',
           width: '100%',
           height: '100%',
@@ -26,10 +49,10 @@ function App() {
         >
         <img src={mainPhoto} className="mainPhoto" alt="Main Photo"/>
 
-        <img src={mic} className="mic" alt="microphone"/>
-          {/* <p>
+        <img src={mic} className="mic" alt="microphone" onClick={handleSubmit}/>
+          <p>
             The current time is {currentTime}.
-          </p> */}
+          </p>
         </div>
 
       </header>
